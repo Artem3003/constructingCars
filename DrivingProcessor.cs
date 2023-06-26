6e39aed0c1ff22992792d224ddffb9485211ff7d
Artem <temapro04@gmai.com>
2023-06-26 19:26:44 +0300

    Implementation of DrivingProcessor

--- DrivingProcessor.cs
+++ DrivingProcessor.cs
@@ -1,28 +1,77 @@
 using System;
 
-namespace constructionCar
+namespace Car
 {
-    public interface IDrivingProcessor
+    interface IDrivingProcessor
     {
-      int ActualSpeed { get; }
+        int ActualSpeed { get; }
 
-      void IncreaseSpeedTo(int speed);
+        void IncreaseSpeedTo(int speed);
 
-      void ReduceSpeed(int speed);
+        void ReduceSpeed(int speed);
     }
 
     class DrivingProcessor : IDrivingProcessor
     {
-        public int ActualSpeed => throw new NotImplementedException();
+        private int maxAcceleration;
+        private int actualSpeed;
+
+        public DrivingProcessor(int maxAcceleration)
+        {
+            this.actualSpeed = 0;
+            this.maxAcceleration = maxAcceleration;
+            if (maxAcceleration > 20)
+            {
+                this.maxAcceleration = 20;
+            }
+            if (maxAcceleration < 5)
+            {
+                this.maxAcceleration = 5;
+            }
+        }
+
+        public int ActualSpeed
+        {
+            get
+            {
+              return this.actualSpeed;
+            }
+        }
 
         public void IncreaseSpeedTo(int speed)
         {
-            throw new NotImplementedException();
+            if (actualSpeed < speed)
+            {
+                actualSpeed += maxAcceleration;
+                if (actualSpeed > speed)
+                {
+                    actualSpeed = speed;
+                }
+            } else
+            {
+                actualSpeed--;
+            }
+            if (actualSpeed > 250)
+            {
+                actualSpeed = 250;
+            }
         }
 
         public void ReduceSpeed(int speed)
         {
-            throw new NotImplementedException();
+            if (actualSpeed > 0)
+            {
+                if (speed == 1)
+                {
+                    actualSpeed--;
+                } else if (actualSpeed - speed < 10)
+                {
+                    actualSpeed -= (actualSpeed - speed);
+                } else if (actualSpeed > speed)
+                {
+                    actualSpeed -= 10;
+                }
+            }
         }
     }
 }
